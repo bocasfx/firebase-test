a0299d62af3844d6725a87159b1855f61ef12c25
Rodolfo Palacios <rpalacios@igloosoftware.com>
2018-10-03 11:07:53 -0400

    Added app version above API response.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { version } from '../package.json';
 
 class App extends Component {
 
@@ -39,9 +40,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          {this.state.response}
-        </p>
+        <div className="App-intro">
+          <h1>App version: {version}</h1>
+          <div>{this.state.response}</div>
+        </div>
       </div>
     );
   }
